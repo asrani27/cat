2eeed8d99a698c48e6c388eb2c6cf41694eef609
asrani <asrani.27@gmail.com>
2022-02-04 18:41:39 +0800

    f

--- resources/views/peserta/sesi2.blade.php
+++ resources/views/peserta/sesi2.blade.php
@@ -10,7 +10,8 @@
     <div class="col-md-12">
         <div class="card card-primary">
             <div class="card-body text-center">
-                Silahkan Download Soal Sesi 2 <a href="/storage/essay.pdf" class="btn btn-sm btn-info">Disini</a>
+                Silahkan Download Soal Sesi 2 <a href="/soal/essay.pdf" target="_blank"
+                    class="btn btn-sm btn-info">Disini</a>
             </div>
         </div>
     </div>
