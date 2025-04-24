f946bb9f688e119c3d31786030cb46e8501e3368
asrani <asrani.27@gmail.com>
2025-04-24 09:29:47 +0800

    f

--- resources/views/layouts/menu_superadmin.blade.php
+++ resources/views/layouts/menu_superadmin.blade.php
@@ -1,65 +1,64 @@
-
 <nav class="mt-2">
-<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-    <li class="nav-item">
-    <a href="/home/superadmin" class="nav-link {{ Request::is('home/superadmin*') ? 'active' : '' }}">
-        <i class="nav-icon fas fa-home"></i>
-        <p>
-        Beranda
-        </p>
-    </a>
-    </li>
-    <li class="nav-item">
-        <a href="/superadmin/peserta" class="nav-link {{ Request::is('superadmin/peserta*') ? 'active' : '' }}">
-            <i class="nav-icon fas fa-users"></i>
-            <p>
-            Peserta
-            </p>
-        </a>
-    </li>
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="/home/superadmin" class="nav-link {{ Request::is('home/superadmin*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-home"></i>
+                <p>
+                    Beranda
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/superadmin/peserta" class="nav-link {{ Request::is('superadmin/peserta*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-users"></i>
+                <p>
+                    Peserta
+                </p>
+            </a>
+        </li>
 
-    <li class="nav-item">
-        <a href="/superadmin/kategori" class="nav-link {{ Request::is('superadmin/kategori*') ? 'active' : '' }}">
-            <i class="nav-icon fas fa-list"></i>
-            <p>
-            Kategori Soal
-            </p>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a href="/superadmin/soal" class="nav-link {{ Request::is('superadmin/soal*') ? 'active' : '' }}">
-            <i class="nav-icon fas fa-list"></i>
-            <p>
-            Soal
-            </p>
-        </a>
-    </li>
-    
-    <li class="nav-item">
-        <a href="/superadmin/waktu" class="nav-link {{ Request::is('superadmin/waktu*') ? 'active' : '' }}">
-            <i class="nav-icon fas fa-clock"></i>
-            <p>
-            Batas Waktu
-            </p>
-        </a>
-    </li>
+        <li class="nav-item">
+            <a href="/superadmin/kategori" class="nav-link {{ Request::is('superadmin/kategori*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-list"></i>
+                <p>
+                    Formasi
+                </p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/superadmin/soal" class="nav-link {{ Request::is('superadmin/soal*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-list"></i>
+                <p>
+                    Soal
+                </p>
+            </a>
+        </li>
 
-    <li class="nav-item">
-        <a href="/superadmin/gantipass" class="nav-link {{ Request::is('superadmin/gantipass*') ? 'active' : '' }}">
-            <i class="nav-icon fas fa-key"></i>
-            <p>
-            Ganti Password
-            </p>
-        </a>
-    </li>
+        <li class="nav-item">
+            <a href="/superadmin/waktu" class="nav-link {{ Request::is('superadmin/waktu*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-clock"></i>
+                <p>
+                    Batas Waktu
+                </p>
+            </a>
+        </li>
 
-    <li class="nav-item">
-    <a href="/logout" class="nav-link">
-        <i class="nav-icon fas fa-sign-out-alt"></i>
-        <p>
-        Logout
-        </p>
-    </a>
-    </li>
-</ul>
+        <li class="nav-item">
+            <a href="/superadmin/gantipass" class="nav-link {{ Request::is('superadmin/gantipass*') ? 'active' : '' }}">
+                <i class="nav-icon fas fa-key"></i>
+                <p>
+                    Ganti Password
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="/logout" class="nav-link">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>
+                    Logout
+                </p>
+            </a>
+        </li>
+    </ul>
 </nav>
