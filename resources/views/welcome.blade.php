76eb0e05f39d378aaf8451502e73531a3932e87b
asrani <asrani.27@gmail.com>
2025-05-07 11:51:14 +0800

    f

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -187,7 +187,7 @@
               </div>
 
               <div class="col-lg-6">
-                <h4 class="text-center">Jadwal Pendaftaran dimulai tanggal : 19 Mei 2025 s/d 21 Mei 2025</h4>
+                <h4 class="text-center">Jadwal Pendaftaran dimulai tanggal : <br /> 19 Mei 2025 s/d 21 Mei 2025</h4>
                 <div class="card card-widget widget-user">
                   <!-- Add the bg color to the header using any of the bg-* classes -->
                   <div class="widget-user-header text-white"
