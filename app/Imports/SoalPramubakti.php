1372e04707fe525a186b50fd2f729debb917ea43
asrani <asrani.27@gmail.com>
2025-05-13 22:17:49 +0800

    f

--- app/Imports/SoalPramubakti.php
+++ app/Imports/SoalPramubakti.php
@@ -20,7 +20,7 @@ class SoalPramubakti implements ToModel, WithStartRow
     public function model(array $row)
     {
         $param['jenis'] = 'TEKNIS';
-        $param['formasi'] = 'PEMULASARAN';
+        $param['formasi'] = 'PRAMUKBAKTI';
         $param['pertanyaan'] = $row[1];
         $param['pil_a'] = $row[2];
         $param['pil_b'] = $row[3];
