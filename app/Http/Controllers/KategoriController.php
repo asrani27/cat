c6eace0894b0795749ba35af2f6c245371f2fb0e
asrani <asrani.27@gmail.com>
2025-06-18 11:04:05 +0800

    f

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -8,18 +8,18 @@ use Illuminate\Support\Facades\Validator;
 
 class KategoriController extends Controller
 {
-    
+
     public function index()
     {
         $data = Kategori::paginate(10);
-        return view('superadmin.kategori.index',compact('data'));
+        return view('superadmin.kategori.index', compact('data'));
     }
-    
+
     public function create()
     {
         return view('superadmin.kategori.create');
     }
-    
+
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -32,35 +32,34 @@ class KategoriController extends Controller
             return back();
         }
         Kategori::create($request->all());
-        
+
         toastr()->success('Sukses Di Simpan');
         return redirect('/superadmin/kategori');
-        
     }
-    
+
     public function show($id)
     {
         //
     }
-    
+
     public function edit($id)
     {
         $data = Kategori::find($id);
-        
-        return view('superadmin.kategori.edit',compact('data'));
+
+        return view('superadmin.kategori.edit', compact('data'));
     }
-    
+
     public function update(Request $request, $id)
     {
-        $validator = Validator::make($request->all(), [
-            'nama' =>  'unique:kategori,nama,'.$id,
-        ]);
+        // $validator = Validator::make($request->all(), [
+        //     'nama' =>  'unique:kategori,nama,'.$id,
+        // ]);
 
-        if ($validator->fails()) {
-            $request->flash();
-            toastr()->error('Kategori sudah ada');
-            return back();
-        }
+        // if ($validator->fails()) {
+        //     $request->flash();
+        //     toastr()->error('Kategori sudah ada');
+        //     return back();
+        // }
 
         $attr = $request->all();
 
