ef750b57dc10f06b01d91813ec60f48a8c6b49a0
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2025-07-04 16:36:30 +0800

    feat: add endpoint for k6 test

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\CheckToken;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\LoginController;
 use App\Http\Controllers\API\PresensiController;
+use App\Http\Controllers\API\TestController;
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -21,3 +22,9 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
 
 // Route::post('/login', [LoginController::class, 'login']);
 // Route::post('/ceklogin', [LoginController::class, 'ceklogin']);
+
+Route::prefix('test')->group(function () {
+    Route::get('/users', [TestController::class, 'getListUser']);
+    Route::get('/users/{user_id}/questions', [TestController::class, 'getSemuaSoalPeserta']);
+    Route::get('/users/{user_id}/questions/{question_id}', [TestController::class, 'soal']);
+});
