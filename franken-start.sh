4e3c8fbefaad9d2faefb6be2986ed63afbc97461
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2025-07-04 22:31:20 +0800

    fix: add workers parameter to octane command in franken-start.sh

--- franken-start.sh
+++ franken-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/php artisan octane:frankenphp --caddyfile Caddyfile --max-requests 0 > /dev/null &
+/usr/bin/php artisan octane:frankenphp --caddyfile Caddyfile --workers $(nproc) --max-requests 0 > /dev/null &
 echo "Service berjalan..."
